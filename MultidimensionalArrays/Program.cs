e93ae82567c580ab15dd2a485e45b2710446d327
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2023-09-26 22:50:26 +0330

    Multidimensional Arrays 2D

---
+++ MultidimensionalArrays/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MultidimensionalArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numbers = new int[2,3] { { 10, 20, 30 }, { 15, 10, 60 } };
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    Console.Write($"|{numbers[i, j]}|");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
