b5b0f98576dc69b6327b91f8382ad96a98f302c3
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2023-09-26 22:58:43 +0330

    Array of Array

---
+++ ArrayOfArray/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ArrayOfArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] playerScores = new int[2][]
+            {
+                new int[] { 50, 70,60},
+                new int[] { 20, 30,40},
+            };
+
+
+            for (int i = 0; i < playerScores.Length; i++)
+            {
+                Console.WriteLine($"Score list player {i + 1}");
+                foreach (var score in playerScores[i])
+                {
+                    Console.WriteLine($"\t{score}");
+                }
+                Console.WriteLine("--------------------");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
