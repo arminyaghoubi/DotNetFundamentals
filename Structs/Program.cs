d5b52fc6acb94c37d6df03f9479e567f8d0ac02b
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2023-09-26 22:13:52 +0330

    Structs

---
+++ Structs/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Structs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your first name: ");
+            string? firstName = Console.ReadLine();
+
+            Console.Write("Please enter your last name: ");
+            string? lastName = Console.ReadLine();
+
+            Student student = new Student(firstName,lastName);
+
+            Console.WriteLine(student);
+
+            Console.ReadKey();
+        }
+    }
+
+    struct Student
+    {
+        public Student(string firstName,string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return $"Full Name: {FirstName} {LastName}";
+        }
+    }
+}
