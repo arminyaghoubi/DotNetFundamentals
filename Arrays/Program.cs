f8c1cc9b59fb212c8abcf11a1f647a1cbf53361b
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2023-09-26 22:26:53 +0330

    Arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"Enter number {i + 1}: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Average is {(float)numbers.Sum() / numbers.Length}");
+
+            Console.ReadKey();
+        }
+    }
+}
