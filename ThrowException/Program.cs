c06007c928af79ae472befdd0ee3da3195970360
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2023-10-04 11:20:24 +0330

    ThrowException

---
+++ ThrowException/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ThrowException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int age = Convert.ToInt32(Console.ReadLine());
+                AgeChacker(age);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+
+        static void AgeChacker(int age)
+        {
+            if (age < 1)
+            {
+                throw new Exception("Age cannot be less than zero");
+            }
+        }
+    }
+}
