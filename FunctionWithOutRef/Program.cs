3c74b035c1687dbe65466e901ebd6034d3a86f7c
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2023-10-04 09:50:05 +0330

    FunctionWithOutRef

---
+++ FunctionWithOutRef/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FunctionWithOutRef
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            SetInitialValue(out number);
+            Console.WriteLine($"Current number is {number}");
+            NextNumber(ref number);
+            Console.WriteLine($"Current number is {number}");
+            Console.ReadKey();
+        }
+
+        static void SetInitialValue(out int number)
+        {
+            number = 10;
+        }
+
+        static void NextNumber(ref int number)
+        {
+            number++;
+        }
+    }
+}
