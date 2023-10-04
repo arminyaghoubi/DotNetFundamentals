ae33b5c7ab0f073579ce55f8b5ccd142fa9b4c29
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2023-10-04 10:23:23 +0330

    StructFunctions

---
+++ ParameterArrays/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ParameterArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintEvenNumbers(1, 2, 3, 4, 5, 6, 7, 8, 9);
+            Console.ReadKey();
+        }
+
+        static void PrintEvenNumbers(params int[] numbers)
+        {
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}
---
+++ StructFunctions/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace StructFunctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee("Armin", "Yaghoubi");
+            employee1.Salary = 2000;
+            employee1.Tax = 20;
+
+            Console.WriteLine(employee1.GetFullName());
+            Console.WriteLine("Income: " + employee1.Income());
+
+            Console.ReadKey();
+        }
+    }
+
+    struct Employee
+    {
+        public Employee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FirstName;
+        public string LastName;
+
+        public int Salary;
+        public int Tax;
+
+        public int Income()
+        {
+            return ((100 - Tax) * Salary) / 100;
+        }
+
+        public string GetFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
