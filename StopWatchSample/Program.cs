417ef215f79394f9dbfbc8d7555120e64500a698
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2023-10-10 11:30:34 +0330

    Stopwatch

---
+++ StopWatchSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace StopWatchSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Thread.Sleep(5000);
+            stopwatch.Stop();
+
+            Console.WriteLine(stopwatch.Elapsed.TotalSeconds);
+        }
+    }
+}
